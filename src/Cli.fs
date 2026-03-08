4a4fec6a52187905e2ba33203e819b8e59578283
Ethan Doughty <etandrew13@gmail.com>
2026-03-07 18:03:29 -0600

    Bump version to 3.3.0 for Sprint Q: arguments block shape extraction, stepped ranges, command syntax, try-parse-retry, 475 tests

--- src/Cli.fs
+++ src/Cli.fs
@@ -285,7 +285,7 @@ let run (argv: string array) : int =
     let args = parseArgv argv
 
     if args.version then
-        printfn "Conformal 3.2.0"
+        printfn "Conformal 3.3.0"
         0
     elif args.help then
         printUsage ()
